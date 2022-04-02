d0f8553053f70d73ea45eeef01a3a33a5fb48b47
IanFla <51277149+IanFla@users.noreply.github.com>
2022-04-02 20:39:57 +0800

    abc

---
+++ analysis.py
@@ -0,0 +1,12 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import pandas as pd
+
+
+def main():
+    data = pd.read_excel('data.xlsx')
+    print(data)
+
+
+if __name__ == '__main__':
+    main()
