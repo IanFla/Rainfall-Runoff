48b6febadd777e77757b115d56d587770a14f70f
IanFla <51277149+IanFla@users.noreply.github.com>
2022-04-18 22:58:19 +0800

    abc

--- analysis2.py
+++ analysis2.py
@@ -9,30 +9,32 @@ plt.rcParams['font.sans-serif'] = ['Arial Unicode MS']
 def read(end, lag):
     length = end - 1962 + 1
     data = pd.read_excel('data.xlsx')
-    flag = (data['year'] >= 1962 - lag) & (data['year'] <= end)
+    flag = (data['year'] >= 1962 - 3) & (data['year'] <= end)
     data = data[flag]
-    t = data['year'].values[lag:]
+    t = data['year'].values[3:]
     x = data['rainfall'].values
-    y = data['runoff'].values[lag:]
-    z = data['mining'].values[lag:]
-    X = np.array([np.flip(x[i:i + lag]) for i in range(length)])
+    y = data['runoff'].values[3:]
+    X = np.array([np.flip(x[i:i + 3]) for i in range(length)])
     X = sm.add_constant(X)
+
+    data2 = pd.read_excel('data.xlsx')
+    flag2 = (data2['year'] >= 1962 - lag) & (data2['year'] <= end)
+    data2 = data2[flag2]
+    z = data2['mining'].values
     Z = np.array([np.flip(z[i:i + lag + 1]) for i in range(length)])
     z_cum = np.array([np.sum(z[:i]) for i in range(length)]).reshape([-1, 1])
-    print(Z)
-    print(X.shape, z_cum.shape, Z.shape)
     return t, np.hstack([X, z_cum, Z]), y
 
 
 def fit(lag):
-    t, X, y, z = read(1979, lag)
-    mod = sm.OLS(y, X)
+    t, XZ, y = read(2020, lag)
+    mod = sm.OLS(y, XZ)
     res = mod.fit()
     return res
 
 
 def draw():
-    lag = np.arange(1, 10)
+    lag = np.arange(0, 10)
     AIC = []
     BIC = []
     R2 = []
@@ -59,10 +61,23 @@ def draw():
 
 
 def main():
-    t, XZ, y = read(2020, 3)
-    print(t)
-    print(y)
-    print(XZ)
+    # draw()
+    res = fit(0)
+    print(res.summary())
+
+    t, XZ, y = read(2020, lag=0)
+    res2 = res.get_prediction(XZ)
+    sigma2 = np.sum((y - res2.predicted_mean) ** 2) / (y.size - XZ.shape[1])
+
+    plt.plot(t, y, 'g', label='实际泉流量')
+    plt.plot(t, res2.predicted_mean, 'r', label='理论泉流量')
+    plt.plot(t, res2.predicted_mean + 1.96 * np.sqrt(sigma2), '--r')
+    plt.plot(t, res2.predicted_mean - 1.96 * np.sqrt(sigma2), '--r')
+    plt.xlabel('年份')
+    plt.ylabel(r'泉流量（$m^3/s$）')
+    plt.legend()
+    plt.tight_layout()
+    plt.show()
 
 
 if __name__ == '__main__':
